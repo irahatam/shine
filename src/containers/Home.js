7973efca2cbdebeb73306f85fde613e181b8077c
Matahari Kinanti Herwin <matahari@nyu.edu>
2020-11-01 23:30:44 -0500

    update

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -77,7 +77,27 @@ function Home() {
             </div>
 
             <div id="programs">
-                <h3>Our Programs</h3>
+                <h3> Programs </h3>
+                <img 
+                    src="https://via.placeholder.com/1200x500" 
+                />
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt.
+                </p>  
+                <img 
+                    src="https://via.placeholder.com/1200x500" 
+                />
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt.
+                </p>                              
             </div>
         </div>
     </main>
